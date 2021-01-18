dc869c89e5092d00503767d0d6e13e4ff4d0dfd8
roxanacaraba <roxana_elena_98@yahoo.com>
2021-01-18 10:36:54 +0200

    Add new files

--- Learning/bucla_infinita.c
+++ Learning/bucla_infinita.c
@@ -2,7 +2,7 @@
 
 int main(){
     int i;
-    for( i = 1; i > 0; i++){
+    for( i = 1; i > 0; i++){ // for ul are 3 pasi, primul pas e asignarea unei valori indexului de iterare, al doilea pas e verificarea conditiei de iesire din for, conditia de iesire reprezinta o operatie de comparare cu o alta valoare in cazul acesta, in general este o operatie logica, iar a treilea pas e incrementarea/decrementarea indexului pentru a parcurge vectorul.
         printf("%d", i*i);
     }
     return 0;
