7f3b4c35b6c4f8365916c265078a0e02b80bd3d1
roxanacaraba <roxana_elena_98@yahoo.com>
2021-01-21 16:27:40 +0200

    add files

--- Sorting-Algorithms/QuickSort4.c
+++ Sorting-Algorithms/QuickSort4.c
@@ -1,38 +1,37 @@
 #include<stdio.h>
 
 int quickSort(int arr[], int start, int end){
-    if(start >= end){
-        return 0;
-    }
-    int pivot = arr[start];
+    int pivot = start;
     int i = start + 1;
     int j = end;
 
-    while( i <= j){
+    if(i < j){
+        if(start >= end){
+            return 0;
+        }
         if(arr[i] < pivot){
             i += 1;
-        }    
+        }
         if(arr[j] > pivot){
             j -= 1;
         }
-        if (i < j && arr[i] > pivot && arr[j] < pivot){
-           int temp = arr[i];
-           arr[i] = arr[j];
-           arr[j] = temp;
-           i += 1;
-           i -= 1;
+        if(i < j && arr[i] > pivot && arr[j] < pivot){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i ++;
+            j--;
         }
     }
+    arr[start] = arr[i -1];
+    arr[i-1] = pivot;
 
-           arr[start] = arr[i-1];
-           arr[i-1] = pivot;
-
-           quickSort(arr, start, i-2);
+ quickSort(arr, start, i-2);
            quickSort(arr, i, end);
         
 }
 int main(){
-   int arr [6] = { 3, 18, 6, 9, 11, 15};
+   int arr [6] = { 10, 2, 16, 8, 12, 15};
    int n = sizeof(arr)/sizeof(arr[0]);
 
    quickSort(arr, 0, n-1);
