39d3290de93ba6e36f2b37eb4bc533113a65cc0d
roxanacaraba <roxana_elena_98@yahoo.com>
2021-02-17 13:29:54 +0200

    add new file

---
+++ String/allPermurtationsOfAGivenString.c
@@ -0,0 +1,32 @@
+//Write a program to print all permutations of a given string.
+
+#include<stdio.h>
+#include<string.h>
+
+void swap(char *x, char *y){ // fuctie pentru interschimbarea valorilor a doi pointeri
+    char temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void permute(char *str, int start, int end){
+    int i; 
+    if(start == end)
+        printf("%s\n", str);
+    else{
+        for(i = start; i <= end; i++){
+            swap((str + start), (str + i));
+            permute(str, start + 1, end);
+            swap((str + start), ( str + i));
+
+        }
+    }
+}
+int main(){
+    char str[5] = "ABC";
+    int len = strlen(str);
+    
+    permute(str, 0, len-1);
+    return 0;
+
+}
